3b962cc4d9df586001b9371e1571bfbf8d39bdf1
yasmeensaber <yasmeensaber94@gmail.com>
2018-01-29 17:34:42 +0200

    2D Game

---
+++ 2D UFO/Assets/Scripts/playerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class playerController : MonoBehaviour {
+	public float speed;
+	public Text scoreText;
+	public Text winText;
+
+	private Rigidbody2D rb;
+	private int count;
+	void Start()
+	{
+		rb = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		SetScoreText ();
+	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		float moveVertical = Input.GetAxisRaw ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal,moveVertical);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetScoreText ();
+		}
+	}
+		void SetScoreText()
+		{
+		scoreText.text = "Score: " + count.ToString ();
+		if (count >= 13) {
+			winText.text="YOU WIN";
+		}
+		}
+	
+}
